6ff53427ac77ce6d061793a8f54e5f97dddcc154
HwangHarim <scopan01@naver.com>
2023-07-24 23:52:08 +0900

    :card_file_box: board domain create

---
+++ src/main/java/com/core/service/board/domain/Board.java
@@ -0,0 +1,34 @@
+package com.core.service.board.domain;
+
+import com.core.service.common.domain.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    private String memberId;
+
+    private String title;
+
+    private String content;
+
+    private Long viewCount;
+}
