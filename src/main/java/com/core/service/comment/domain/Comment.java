4ca04fd0bdb62ac285922a264dce53b2dd5b6bbd
HwangHarim <scopan01@naver.com>
2023-07-26 23:26:32 +0900

    :card_file_box: comment domain

---
+++ src/main/java/com/core/service/comment/domain/Comment.java
@@ -0,0 +1,30 @@
+package com.core.service.comment.domain;
+
+import com.core.service.common.domain.BaseEntity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String memberId;
+
+    private Long boardId;
+
+    private String comment;
+}
