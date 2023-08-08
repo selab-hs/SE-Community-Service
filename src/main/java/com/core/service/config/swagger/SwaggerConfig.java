93a445face46357d7fd4e033d4f41a289b44e9fd
InJun2 <os2864@naver.com>
2023-08-08 22:32:14 +0900

    :sparkles: swagger 설정 파일 생성

---
+++ src/main/java/com/core/service/config/swagger/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.core.service.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("SE Community Service")
+                .version("v0.0.1")
+                .description("SE Community Web Service API 구현");
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
