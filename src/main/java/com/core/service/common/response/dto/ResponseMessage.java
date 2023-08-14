ed198202c32606d2b415a0ed4bac0235f34cee99
InJun2 <os2864@naver.com>
2023-08-14 22:09:39 +0900

    :sparkles: ResponseEntity 메시지로 사용할 ResponseMessage 추가

--- src/main/java/com/core/service/common/response/dto/ResponseMessage.java
+++ src/main/java/com/core/service/common/response/dto/ResponseMessage.java
@@ -6,7 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ResponseMessage {
 
-    SUCCESS(HttpStatus.OK,"SUCCESS");
+    SUCCESS(HttpStatus.OK,"SUCCESS"),
+    CREATE_SUCCESS_MEMBER(HttpStatus.CREATED, "멤버 회원 가인 성공"),
+    SUCCESS_LOAD_MEMBER_INFORMATION(HttpStatus.OK, "회원 정보 조회 성공"),
+    SUCCESS_SEARCH_ALL_MEMBER(HttpStatus.OK, "모든 회원 조회 성공");
 
     public final static String SUCCESS_MESSAGE = "SUCCESS";
     private final static String NOT_FOUND_MESSAGE = "NOT FOUND";
