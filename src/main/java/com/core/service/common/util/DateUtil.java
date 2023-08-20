8c7484f5d73a4174351d5e2efec7423bb6a54706
InJun2 <os2864@naver.com>
2023-08-21 00:39:48 +0900

    :sparkles: DateUtil 생성

---
+++ src/main/java/com/core/service/common/util/DateUtil.java
@@ -0,0 +1,13 @@
+package com.core.service.common.util;
+
+import java.util.Date;
+
+public class DateUtil {
+    public static Date getDate(){
+        return new Date();
+    }
+
+    public static Date getTokenValidTime(Date date, Long validTime){
+        return new Date(date.getTime() + validTime);
+    }
+}
