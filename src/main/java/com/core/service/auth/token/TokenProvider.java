e200c7b09a3d71ad7fa41493f509d867cb3c078e
InJun2 <os2864@naver.com>
2023-08-21 00:43:19 +0900

    :sparkles: jwt tokenProvider 구현

---
+++ src/main/java/com/core/service/auth/token/TokenProvider.java
@@ -0,0 +1,114 @@
+package com.core.service.auth.token;
+
+import com.core.service.auth.application.AuthService;
+import com.core.service.auth.domain.Authentication;
+import com.core.service.auth.domain.UserDetail;
+import com.core.service.common.util.DateUtil;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class TokenProvider {
+    private static final String AUTHORITIES_KEY = "roles";
+
+    @Value("${jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.token.expiry}")
+    private Long tokenExpiry;
+
+    @Value("${jwt.token.refresh-expiry}")
+    private Long refreshTokenExpiry;
+
+    private final AuthService authService;
+
+    public TokenProvider(@Lazy AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostConstruct
+    public void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    /**
+     * @brief      * 토큰 생성 / jjwt 라이브러리
+     * @param      * claim : 데이터 (String userId)
+     *                  * Subject : 토큰 생성 목적
+     *             * IssuedAt : jwt 발급 시간
+     *             * Expiration Time : jwt 만료시간
+     *             * signWith : 암호화 알고리즘, secret 값 세팅
+     *             *
+     * @return     * access Jwt Token
+     */
+    public String generateJwtToken(Long userId, String role){
+        var claims = Jwts.claims().setSubject(String.valueOf(userId));
+        claims.put(AUTHORITIES_KEY, role);
+        var date = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(date)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setExpiration(DateUtil.getTokenValidTime(date, tokenExpiry))
+                .compact();
+    }
+
+    /**
+     * @brief      * 토큰 인증 정보 조회
+     * @param      * getUserToken : 파싱된 토큰
+     * @return     * Authentication : 유저 정보와 유저 역할
+     */
+    public Authentication getAuthentication(String token) {
+        UserDetail userDetail = authService.loadUserById(this.getUserToken(token));
+        return new Authentication(userDetail, userDetail.getRoleType());
+    }
+
+    /**
+     * @brief      * 토큰 파싱 (해석) / jjwt 라이브러리
+     * @param      * setSigningKey : jwt key
+     *             * parseClaimsJws : 파싱할 token
+     *             * Body : claims
+     *             * Subject : userId
+     * @return     * Long userId
+     */
+    public Long getUserToken(String token) {
+        var data = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        return Long.parseLong(data);
+    }
+
+    /**
+     * @brief      * 토큰 기간 유효성 검사
+     * @param      * setSigningKey : jwt key
+     *             * parseClaimsJws : 파싱할 token
+     * @return     * token 기간
+     */
+
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return claims.getBody().getExpiration().after(new Date());
+        } catch (ExpiredJwtException e)
+        {
+            log.error("토큰 만료");
+            return false;
+        }
+        catch (JwtException e)
+        {
+            log.error("jwt 에러");
+            return false;
+        }
+    }
+}
