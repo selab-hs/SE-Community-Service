ff8955e6d77fcb2808a549c3c6040bb28e478232
InJun2 <os2864@naver.com>
2023-08-21 01:57:37 +0900

    :sparkles: customArgumentResolver 설정 추가

--- src/main/java/com/core/service/config/web/WebConfig.java
+++ src/main/java/com/core/service/config/web/WebConfig.java
@@ -1,16 +1,22 @@
 package com.core.service.config.web;
 
+import com.core.service.auth.presentation.AuthArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private  final AuthArgumentResolver authArgumentResolver;
 
     @Bean
     @ConditionalOnMissingBean(UrlBasedCorsConfigurationSource.class)
@@ -27,5 +33,10 @@ public class WebConfig implements WebMvcConfigurer {
         var corsConfigSource = new UrlBasedCorsConfigurationSource();
         corsConfigSource.registerCorsConfiguration("/**", corsConfig);
         return corsConfigSource;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
     }
 }
