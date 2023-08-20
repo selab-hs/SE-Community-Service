ddfb2c110805ce37cd0b84433af2d8c7848f1084
HwangHarim <scopan01@naver.com>
2023-08-21 00:10:50 +0900

    :recycle: request set 메서드 추가

--- src/main/java/com/core/service/board/dto/request/CreateBoardRequest.java
+++ src/main/java/com/core/service/board/dto/request/CreateBoardRequest.java
@@ -2,8 +2,10 @@ package com.core.service.board.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class CreateBoardRequest {
     private Long id;
     private String memberId;
