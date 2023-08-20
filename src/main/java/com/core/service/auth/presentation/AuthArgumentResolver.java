7b2ba2130fc5be11bf9b196deb95cd38fbd9e237
InJun2 <os2864@naver.com>
2023-08-21 00:47:22 +0900

    :sparkles: AuthMember 어노테이션 연결

---
+++ src/main/java/com/core/service/auth/presentation/AuthArgumentResolver.java
@@ -0,0 +1,44 @@
+package com.core.service.auth.presentation;
+
+import com.core.service.auth.domain.UserDetail;
+import com.core.service.auth.infrastructure.annotation.AuthMember;
+import com.core.service.auth.infrastructure.authentication.DefaultAuthenticationStrategy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+    private final DefaultAuthenticationStrategy defaultAuthenticationStrategy;
+
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return Objects.nonNull(parameter.getParameterAnnotation(AuthMember.class));
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer
+            , NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        var httpServletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        var userData = userInfo(httpServletRequest);
+        if (parameter.getParameterType() == Optional.class) {
+            return userData;
+        }
+        return userData.orElseThrow(NullPointerException::new);
+    }
+
+    private Optional<UserDetail> userInfo(HttpServletRequest request){
+        var userDetail = defaultAuthenticationStrategy.get(request).userDetail();
+        return Optional.ofNullable(userDetail);
+    }
+}
