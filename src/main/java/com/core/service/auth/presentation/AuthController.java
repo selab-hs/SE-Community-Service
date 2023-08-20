c78a6de3691259c6b3ec5576081a2d6a7256ca54
InJun2 <os2864@naver.com>
2023-08-21 00:44:09 +0900

    :sparkles: login 구현 및 jwt 생성 및 반환

---
+++ src/main/java/com/core/service/auth/presentation/AuthController.java
@@ -0,0 +1,26 @@
+package com.core.service.auth.presentation;
+
+import com.core.service.auth.application.AuthService;
+import com.core.service.auth.dto.request.JoinRequest;
+import com.core.service.common.response.dto.ResponseDto;
+import com.core.service.common.response.dto.ResponseMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDto> login(@RequestBody JoinRequest request) {
+        var userDetail = authService.userLogin(request);
+
+        return ResponseDto.toResponseEntity(ResponseMessage.SUCCESS_LOAD_MEMBER_INFORMATION, userDetail);
+    }
+
+
+}
