5a869c48e48bb66e4f0fe86436edd0da41239ba0
InJun2 <os2864@naver.com>
2023-08-21 00:34:30 +0900

    :sparkles: member ResponseMessage 추가

--- src/main/java/com/core/service/common/response/dto/ResponseMessage.java
+++ src/main/java/com/core/service/common/response/dto/ResponseMessage.java
@@ -8,6 +8,12 @@ public enum ResponseMessage {
 
     SUCCESS(HttpStatus.OK,"SUCCESS"),
 
+    // 회원 성공 message
+
+    CREATE_SUCCESS_MEMBER(HttpStatus.CREATED, "멤버 회원 가입 성공"),
+    SUCCESS_LOAD_MEMBER_INFORMATION(HttpStatus.OK, "회원 정보 조회 성공"),
+    SUCCESS_SEARCH_ALL_MEMBER(HttpStatus.OK, "모든 회원 조회 성공"),
+
     // 게시판 성공 message
     CREATE_SUCCESS_BOARD(HttpStatus.CREATED,"Board를 생성 했습니다."),
     UPDATE_SUCCESS_BOARD(HttpStatus.OK,"Board를 수정 하였습니다."),
@@ -15,7 +21,8 @@ public enum ResponseMessage {
     READ_SUCCESS_ALL_BOARD(HttpStatus.OK,"전체 Board 조회를 성공 했습니다."),
 
     READ_SUCCESS_BOARD_VIEW_COUNT(HttpStatus.OK,"해당 Board 조회수 조회를 성공 했습니다."),
-    DELETE_SUCCESS_BOARD(HttpStatus.OK,"해당 Board 삭제 성공 했습니다.")
+    DELETE_SUCCESS_BOARD(HttpStatus.OK,"해당 Board 삭제 성공 했습니다."),
+
     ;
 
     public final static String SUCCESS_MESSAGE = "SUCCESS";
