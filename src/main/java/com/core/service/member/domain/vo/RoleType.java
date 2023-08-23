6bb0f8364f62ddd010ef0edc794fcee6783ed102
InJun2 <os2864@naver.com>
2023-08-23 23:17:22 +0900

    :bug: 사용하지 않는 역할 제거

--- src/main/java/com/core/service/member/domain/vo/RoleType.java
+++ src/main/java/com/core/service/member/domain/vo/RoleType.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 public enum RoleType {
     GUEST("GUEST", "게스트"),
     LAB_USER("ROLE_USER", "랩원"),
-    LAB_MANAGER("ROLE_MANAGER", "랩총무"),
     LAB_LEADER("ROLE_LEADER", "랩장");
 
     private final String role;
