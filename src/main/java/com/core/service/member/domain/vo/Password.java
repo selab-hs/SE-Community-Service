e2b686b2191ced414181607ba1a37dc0f91a48e4
InJun2 <os2864@naver.com>
2023-08-23 23:37:50 +0900

    :art: Converter 사용을 위한 기본 생성자 제거 및 예외처리 수정

--- src/main/java/com/core/service/member/domain/vo/Password.java
+++ src/main/java/com/core/service/member/domain/vo/Password.java
@@ -1,14 +1,12 @@
 package com.core.service.member.domain.vo;
 
 import com.core.service.error.dto.ErrorMessage;
-import com.core.service.error.exception.member.InvalidEmailException;
+import com.core.service.error.exception.member.InvalidPasswordException;
 import lombok.Getter;
 
-import javax.persistence.Embeddable;
 import java.util.regex.Pattern;
 
 @Getter
-@Embeddable
 public class Password {
     private static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$";
     private String password;
@@ -17,13 +15,10 @@ public class Password {
         validatePassword(password);
     }
 
-    protected Password() {
-    }
-
     private void validatePassword(String password) {
         if(!Pattern.matches(PASSWORD_REGEX, password)){
-            throw new InvalidEmailException(ErrorMessage.INVALID_PASSWORD_REGEX_EXCEPTION, "유효한 비밀번호 형식이 아닙니다");
-        };
+            throw new InvalidPasswordException(ErrorMessage.INVALID_PASSWORD_REGEX_EXCEPTION, "유효한 비밀번호 형식이 아닙니다");
+        }
         this.password = password;
     }
 
