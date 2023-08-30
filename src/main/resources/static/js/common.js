284e1fad3ef1f62ce492d6008f26fbd817b881cb
InJun2 <os2864@naver.com>
2023-08-30 16:06:41 +0900

    :sparkles: footer 생성 js 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,3 +2,8 @@ $(document).ready(function () {
     $("#header").load("../header/header.html", function () {
     });
 })
+
+$(document).ready(function () {
+    $("#footer").load("../footer/footer.html", function () {
+    });
+})
