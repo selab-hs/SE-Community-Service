9b64e4c9b0ba579772fe536f52918bb3e7ed7c2e
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-10-03 15:09:32 +0900

    feat: health check api

---
+++ src/main/java/com/core/service/health/presentation/HealthController.java
@@ -0,0 +1,12 @@
+package com.core.service.health.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "health good!~";
+    }
+}
