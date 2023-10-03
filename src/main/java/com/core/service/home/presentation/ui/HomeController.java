af83d8bf0277976405c88099a88369a73a00d40e
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-10-03 16:03:27 +0900

    feat: home ui

---
+++ src/main/java/com/core/service/home/presentation/ui/HomeController.java
@@ -0,0 +1,12 @@
+package com.core.service.home.presentation.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "index";
+    }
+}
