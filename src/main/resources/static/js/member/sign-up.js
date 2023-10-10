31df8ab0f577765ef3ee71273fcc462d816c96ae
InJun2 <os2864@naver.com>
2023-10-10 17:16:46 +0900

    :sparkles: 이메일 중복 확인 등의 유효성 검사 및 회원가입 요청 전송

--- src/main/resources/static/js/member/sign-up.js
+++ src/main/resources/static/js/member/sign-up.js
@@ -1,73 +1,108 @@
 // 이메일 방식 입력
-var emailCheck = /[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/;
+const emailCheck = /[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/;
 
- // 비밀번호 영문자+숫자+특수조합(8~16자리 입력) 정규식
-var pwdCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+// 비밀번호 영문자+숫자+특수조합(8~16자리 입력) 정규식
+const pwdCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
 
 // 이름 한글로 1~5 글자 입력
-var nameCheck = /^[ㄱ-ㅎ가-힣]{1,5}$/;
+const nameCheck = /^[ㄱ-ㅎ가-힣]{1,5}$/;
 
 // 숫자 2개 + '학번' 입력
-var studentIdCheck = /^[0-9]{2}학번$/;
+const studentIdCheck = /^[0-9]{2}학번$/;
+
+let email_boolean = false;
+let email_duplication_boolean = false;
+let password_boolean = false;
+let password_check_boolean = false;
+let name_boolean = false;
+let student_boolean = false;
 
 function email_check() {
-    var email = document.getElementById("email_input");
-    var email_error_text = document.getElementById("email_error_text");
+    const email = document.getElementById("email_input");
+    const email_error_text = document.getElementById("email_error_text");
 
     if(!emailCheck.test(email.value)) {
         email_error_text.style.display = 'block';
-        return false;
+        email_boolean =  false;
     } else {
         email_error_text.style.display = 'none';
+        email_boolean = true;
     }
 }
 
+function email_duplication_check() {
+    const email = $("#email_input");
+    if(!email_boolean) {
+        alert("유효하지 않은 이메일 형식입니다");
+    }
+
+    $.ajax({
+        url: "http://localhost:8080/api/v1/members/email-check?email=" + email.val(),
+        type: "GET",
+        success: function() {
+            alert("중복되지 않은 이메일입니다.");
+            email_duplication_boolean = true;
+            email.prop('readonly', true);
+            email.css("background-color", "#E6E6E6");
+            $(".join_email").css("background-color", "#E6E6E6");
+        },
+        error: function(){
+            alert("중복된 이메일입니다. 다른 이메일을 입력해주세요.");
+            email_duplication_boolean = false;
+        }
+    })
+}
+
 function pwd_check() {
-    var pwd = document.getElementById("password_input");
-    var pwd_error_text = document.getElementById("password_error_text");
+    const pwd = document.getElementById("password_input");
+    const pwd_error_text = document.getElementById("password_error_text");
 
     if(!pwdCheck.test(pwd.value)) {
         pwd_error_text.style.display = 'block';
-        return false;
+        password_boolean = false;
     } else {
         pwd_error_text.style.display = 'none';
+        password_boolean = true;
     }
 }
 
 function pwd_check2() {
-    var pwd = document.getElementById("password_input");
-    var pwd_check = document.getElementById("password_check_input");
-    var pwd_check_error_text = document.getElementById("password_check_error_text");
+    const pwd = document.getElementById("password_input");
+    const pwd_check = document.getElementById("password_check_input");
+    const pwd_check_error_text = document.getElementById("password_check_error_text");
 
     if(pwd.value !== pwd_check.value) {
         pwd_check_error_text.style.display = 'block';
-        return false;
+        password_check_boolean = false;
     } else {
         pwd_check_error_text.style.display = 'none';
+        password_check_boolean = true;
     }
 }
 
 function name_check() {
-    var name = document.getElementById("name_input");
-    var name_error_text = document.getElementById("name_error_text");
+    const name = document.getElementById("name_input");
+    const name_error_text = document.getElementById("name_error_text");
 
     if(!nameCheck.test(name.value)) {
         name_error_text.style.display = 'block';
-        return false;
+        name_boolean = false;
     } else {
         name_error_text.style.display = 'none';
+        name_boolean = true;
     }
 }
 
 function student_id_check() {
-    var student_id = document.getElementById("student_id_input");
-    var student_id_error_text = document.getElementById("student_id_error_text");
+    const student_id = document.getElementById("student_id_input");
+    const student_id_error_text = document.getElementById("student_id_error_text");
 
     if(!studentIdCheck.test(student_id.value)) {
         student_id_error_text.style.display = 'block';
-        return false;
+        student_boolean = false;
     } else {
         student_id_error_text.style.display = 'none';
+        student_boolean = true;
     }
 }
 
@@ -96,3 +131,33 @@ document.getElementById("name_input").addEventListener("blur", function() {
 document.getElementById("student_id_input").addEventListener("blur", function() {
     student_id_check();
 });
+
+function sing_up_submit() {
+    if(email_duplication_boolean && password_boolean && password_check_boolean
+        && name_boolean && student_boolean) {
+        let data = JSON.stringify({
+            "email" :  $("#email_input").val(),
+            "password" : $("#password_input").val(),
+            "name" : $("#name_input").val(),
+            "grade" : $("#grade_select").val(),
+            "studentId" : $("#student_id_input").val()
+        });
+
+        $.ajax({
+            url: "http://localhost:8080/api/v1/members/signup",
+            data: data,
+            contentType: 'application/json',
+            type: "POST",
+            success: function() {
+                alert("회원 가입 성공 !");
+                location.href = "http://localhost:8080/login";
+            },
+            error: function(request){
+                alert("code = "+ request.status + "\nmessage = " + request.responseText);
+            }
+        })
+    } else {
+        alert("회원가입 기입이 잘못되었거나 작성되지 않았습니다.");
+        return false;
+    }
+}
