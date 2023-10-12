5fd0e0dc7c50a317b09b72b3b500ad79f8aaa0ea
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-10-12 19:12:32 +0900

    fix: 댓글 도메인에서 boardId 제거

--- src/main/java/com/hs/selab/comment/domain/Comment.java
+++ src/main/java/com/hs/selab/comment/domain/Comment.java
@@ -2,15 +2,17 @@ package com.hs.selab.comment.domain;
 
 import com.hs.selab.comment.dto.request.UpdateCommentRequest;
 import com.hs.selab.common.domain.BaseEntity;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Getter
@@ -23,15 +25,15 @@ public class Comment extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "member_id")
     private Long memberId;
 
-    private Long boardId;
-
+    @Column(name = "post_id")
     private Long postId;
 
     private String comment;
 
-    public void update(UpdateCommentRequest updateCommentRequest){
+    public void update(UpdateCommentRequest updateCommentRequest) {
         this.comment = updateCommentRequest.getComment();
     }
 }
--- src/main/java/com/hs/selab/comment/domain/converter/CommentConverter.java
+++ src/main/java/com/hs/selab/comment/domain/converter/CommentConverter.java
@@ -4,32 +4,32 @@ import com.hs.selab.auth.domain.UserDetail;
 import com.hs.selab.comment.domain.Comment;
 import com.hs.selab.comment.dto.request.CreateCommentRequest;
 import com.hs.selab.comment.dto.response.ReadCommentResponse;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentConverter {
 
 
-    public Comment convertToEntity(CreateCommentRequest request, UserDetail userInfo){
+    public Comment convertToEntity(CreateCommentRequest request, UserDetail userInfo) {
         return Comment.builder()
-            .memberId(userInfo.getId())
-            .boardId(request.getBoardId())
-            .postId(request.getPostId())
-            .comment(request.getComment())
-            .build();
+                .memberId(userInfo.getId())
+                .postId(request.getPostId())
+                .comment(request.getComment())
+                .build();
     }
 
-    public List<ReadCommentResponse> convertToReadCommentResponse(List<Comment> comments){
+    public List<ReadCommentResponse> convertToReadCommentResponse(List<Comment> comments) {
         return comments.stream().map(
-          comment -> ReadCommentResponse.builder()
-              .commentId(comment.getId())
-              .memberId(comment.getMemberId())
-              .boardId(comment.getBoardId())
-              .postId(comment.getPostId())
-              .comment(comment.getComment())
-              .build()
+                comment -> ReadCommentResponse.builder()
+                        .commentId(comment.getId())
+                        .memberId(comment.getMemberId())
+                        .postId(comment.getPostId())
+                        .comment(comment.getComment())
+                        .build()
         ).collect(Collectors.toList());
     }
 }
