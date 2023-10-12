2bf4fb0afb017774f82da6ed8a5578ca810b5ff5
InJun2 <os2864@naver.com>
2023-10-12 15:42:11 +0900

    :sparkles: 로그인 토큰 여부 확인 로직 및 잘못된 정보 로그인 실패 모달 추가

--- src/main/resources/static/js/member/login.js
+++ src/main/resources/static/js/member/login.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.localStorage.getItem('X-SELAB-AUTH-TOKEN') != null) {
+        alert('유저 정보가 이미 존재합니다.');
+        // 여기에 로컬 스토리지 토큰 삭제 추가
+        location.href = "http://localhost:8080/home";
+    }
+});
+
 function login_submit() {
     let data = JSON.stringify({
         "email" :  $("#login_id").val(),
@@ -10,19 +18,16 @@ function login_submit() {
         contentType: 'application/json',
         type: "POST",
         success: function(result) {
-            alert("성공 : " + result.data);
+            alert("로그인 성공!");
             window.localStorage.setItem("X-SELAB-AUTH-TOKEN", result.data);
+            location.href = "http://localhost:8080/home";
         },
         error: function(request){
-            alert("code = "+ request.status + "\nmessage = " + request.responseText);
+            if(request.status === 400) {
+                alert("로그인 정보가 잘못되었습니다");
+            } else {
+                alert("code = "+ request.status + "\nmessage = " + request.responseText);
+            }
         }
     })
-}
-
-function check_localStorage() {
-    alert(window.localStorage.getItem("X-SELAB-AUTH-TOKEN"));
-}
-
-function clear_localStorage() {
-    window.localStorage.clear();
 }
