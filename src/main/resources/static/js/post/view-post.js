8c2fd3c227430b5e736291f4c18c42b0023ec6e5
HwangHarim <scopan01@naver.com>
2023-10-17 00:39:35 +0900

    :art: post view 페이지 연결

--- src/main/resources/static/js/post/view-post.js
+++ src/main/resources/static/js/post/view-post.js
@@ -22,21 +22,71 @@ document.addEventListener('DOMContentLoaded', function () {
         'X-SELAB-AUTH-TOKEN': authToken
       },
       success: function (result) {
-        $('#post_title').val(result.data.title);
+        $('#post_title').text(result.data.title);
+        $('#post_writer').text(result.data.memberId);
         $('#post_content').val(result.data.content);
       }
     });
-    const urlParams = new URLSearchParams("?boardId="+contestPath)
+    const urlParams = new URLSearchParams("?postId="+contestPath)
     $.ajax({
-      url: 'http://localhost:8080/api/v1/comments/'+ contestPath,
+      url: 'http://localhost:8080/api/v1/comments?'+ urlParams,
       type: 'GET',
       headers: {
         'X-SELAB-AUTH-TOKEN': authToken
       },
       success: function (result) {
-        $('#comment_member_id_title').val(result.data.title);
-        $('#post_content').val(result.data.content);
-      }
-    });
+        var arr = result.data;
+        var commentsContainer = $('#comments');
+
+        for (var i = arr.length-1; 0<=i ; i--) {
+          // Create elements
+          var ul = $('<ul>').addClass('posts');
+          var li = $('<li>');
+          var article = $('<article>');
+          var header = $('<header>');
+          var h3 = $('<h3>').attr('id', 'comment_member_id').text(arr[i].comment);
+          var p = $('<p>').addClass('published').attr('id', 'comment_content').text(arr[i].commentId);
+          var a = $('<a>').attr('href', 'page/single.html').addClass('image');
+          var img = $('<img>').attr('src', '/images/pic08.jpg').attr('alt', '');
+
+          // Append elements
+          header.append(h3);
+          header.append(p);
+          a.append(img);
+          article.append(header);
+          article.append(a);
+          li.append(article);
+          ul.append(li);
+
+          // Append the created elements to the commentsContainer
+          commentsContainer.append(ul);
+        }
+    }})
   }
 });
+
+function create_comment_submit(){
+  const pathArray = window.location.pathname.split("/");
+  const contestPath = pathArray[2];
+  var authToken = window.localStorage.getItem('X-SELAB-AUTH-TOKEN');
+  let data = JSON.stringify({
+        "postId" : contestPath,
+        "comment" : $("#comment-message").val()
+      })
+
+  $.ajax({
+    url: "http://localhost:8080/api/v1/comments",
+    data: data,
+    headers : {
+      'Content-Type': 'application/json',
+      'X-SELAB-AUTH-TOKEN': authToken
+    },
+    type: "POST",
+    success: function() {
+      location.href = "http://localhost:8080/viewPost/"+ contestPath;
+    },
+    error: function(request){
+      alert("code = "+ request.status + "\nmessage = " + request.responseText);
+    }
+  });
+}
