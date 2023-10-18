1e7ff307b4a252e3f2c8125442266321c0b8496c
InJun2 <os2864@naver.com>
2023-10-18 19:55:36 +0900

    :sparkles: Jwt 예외 추가

--- src/main/java/com/hs/selab/error/exception/member/SeJwtException.java
+++ src/main/java/com/hs/selab/error/exception/member/SeJwtException.java
@@ -3,16 +3,16 @@ package com.hs.selab.error.exception.member;
 import com.hs.selab.error.dto.ErrorMessage;
 import com.hs.selab.error.exception.BusinessException;
 
-public class ExpiredJwtException extends BusinessException {
-    public ExpiredJwtException(ErrorMessage message) {
+public class SeJwtException extends BusinessException {
+    public SeJwtException(ErrorMessage message){
         super(message);
     }
 
-    public ExpiredJwtException(ErrorMessage message, String reason) {
+    public SeJwtException(ErrorMessage message, String reason) {
         super(message, reason);
     }
 
-    public ExpiredJwtException(String reason) {
+    public SeJwtException(String reason) {
         super(reason);
     }
 }
