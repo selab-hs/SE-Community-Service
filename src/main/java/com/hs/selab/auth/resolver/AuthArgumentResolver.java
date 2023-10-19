f153aad5681c70de8df4cd0040cbd21959a50e36
InJun2 <os2864@naver.com>
2023-10-19 15:04:29 +0900

    :art: 제거한 filter 로직추가 및 contextHolder 제거

--- src/main/java/com/hs/selab/auth/resolver/AuthArgumentResolver.java
+++ src/main/java/com/hs/selab/auth/resolver/AuthArgumentResolver.java
@@ -1,8 +1,11 @@
 package com.hs.selab.auth.resolver;
 
+import com.hs.selab.auth.domain.Authentication;
 import com.hs.selab.auth.domain.UserDetail;
 import com.hs.selab.auth.infrastructure.annotation.AuthMember;
-import com.hs.selab.auth.infrastructure.authentication.DefaultAuthenticationStrategy;
+import com.hs.selab.auth.token.TokenProvider;
+import com.hs.selab.common.header.HeaderUtil;
+import com.hs.selab.member.domain.vo.RoleType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -10,7 +13,6 @@ import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.Objects;
 import java.util.Optional;
@@ -18,8 +20,7 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
-    private final DefaultAuthenticationStrategy defaultAuthenticationStrategy;
-
+    private final TokenProvider tokenProvider;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -30,14 +31,17 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer
             , NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         var httpServletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-        var userData = userInfo(httpServletRequest);
-        if (parameter.getParameterType() == Optional.class) {
-            return userData;
-        }
-        return userData.orElseThrow(NullPointerException::new);
-    }
+        var token = HeaderUtil.getAccessToken(httpServletRequest);
+        UserDetail userData = null;
 
-    private Optional<UserDetail> userInfo(HttpServletRequest request){
-        return Optional.of(defaultAuthenticationStrategy.get(request).getUserDetail());
+        if (token != null && tokenProvider.validateDateToken(token)) {
+            userData = tokenProvider.getAuthentication(token).getUserDetail();
+        }
+
+        if(token == null){
+            userData = new Authentication(new UserDetail(), RoleType.GUEST).getUserDetail();
+        }
+
+        return Optional.of(userData).orElseThrow(NullPointerException::new);
     }
 }
