bda3ef1096cc3e43ba503b8bdcfe79b05c861545
InJun2 <os2864@naver.com>
2023-10-20 23:00:41 +0900

    :fire: 주석제거

--- src/main/resources/static/js/member/login.js
+++ src/main/resources/static/js/member/login.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     if (window.localStorage.getItem('X-SELAB-AUTH-TOKEN') != null) {
         alert('유저 정보가 이미 존재합니다.');
-        // 여기에 로컬 스토리지 토큰 삭제 추가
         location.href = "http://localhost:8080/home";
     }
 });
