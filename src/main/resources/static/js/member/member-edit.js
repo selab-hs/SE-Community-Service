204f7818be8cc0cc09e850438728cb16db449418
InJun2 <os2864@naver.com>
2023-10-20 23:02:28 +0900

    :fire: 주석제거

--- src/main/resources/static/js/member/member-edit.js
+++ src/main/resources/static/js/member/member-edit.js
@@ -106,7 +106,6 @@ function edit_submit() {
 document.addEventListener("DOMContentLoaded", function() {
     if(window.localStorage.getItem('X-SELAB-AUTH-TOKEN') == null) {
         alert('유저 토큰이 존재하지 않습니다');
-        // 여기에 로컬 스토리지 토큰 삭제 추가
         location.href = "http://localhost:8080/login";
         return;
     }
